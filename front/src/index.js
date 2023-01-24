3381cbed248da9420da093810a999c074000b77a
hwanny7 <asdryzx2@gmail.com>
2023-01-24 20:29:27 +0900

    last

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// store 연결, logout이랑 private router 부분 확인하기
+
 ReactDOM.render(
   <BrowserRouter>
     <App/>
